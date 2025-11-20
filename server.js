8810befaf14bf285a1e33d7bc5bcd34a12735233
noorqawasmeh <93261029+noorqawasmeh@users.noreply.github.com>
2025-11-20 03:24:06 +0300

    Set up Pug view engine and index route rendering
    Added Pug view engine setup and rendering for the index route.

--- server.js
+++ server.js
@@ -6,13 +6,17 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+// Set up Pug view engine
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/views/pug'); // IMPORTANT
+
+fccTesting(app); // For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render('index');  // FCC wants simple render
 });
 
 const PORT = process.env.PORT || 3000;
